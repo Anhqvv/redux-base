2234f658cce39028d73384b79a2d2f85d96b3c7c
Anhqvv <anhqvv2022@gmail.com>
2022-12-26 16:22:00 +0700

    #1 Setup Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+import "./App.css"
+
+import { connect } from "react-redux"
+
+import {
+  increaseCounter,
+  decreaseCounter,
+} from "./action/actions"
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
-      </header>
+      <div>Count: {props.count}</div>
+
+      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+
+      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
     </div>
-  );
+  )
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.counter.count,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
