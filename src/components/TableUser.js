b6b160e23cbdfef857cf046b06c66bcfd991ba49
Anhqvv <anhqvv2022@gmail.com>
2022-12-30 19:47:28 +0700

    #3 Setup before FetchData

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,33 +1,54 @@
 import Table from 'react-bootstrap/Table'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 const TableUser = () => {
+  const [listUsers, setListUsers] = useState('')
+  const fetchAllUser = async () => {
+    let res = await axios.get('http://localhost:8080/users/all')
+    const data = res && res.data ? res.data : []
+    setListUsers(data)
+    console.log('object:', data)
+  }
+
+  const handleDeleteUser = user => {
+    console.log('Checking delete user:', user)
+  }
+  useEffect(() => {
+    fetchAllUser()
+  }, [])
+
   return (
-    <>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-        </tbody>
-      </Table>
-    </>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Email address</th>
+          <th>Username</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUsers &&
+          listUsers.length > 0 &&
+          listUsers.map((item, index) => {
+            return (
+              <tr key={`users-${index}`}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.username}</td>
+                <td>
+                  <button
+                    className='btn btn-danger'
+                    onClick={() => handleDeleteUser(item)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+      </tbody>
+    </Table>
   )
 }
 
